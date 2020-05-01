9985ebfa96319550748a4f5486e839aff727d5d4
WinstonPais <51374812+WinstonPais@users.noreply.github.com>
2020-05-01 15:50:13 +0530

    basic register and login

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .forms import UserForm
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
 # Create your views here.
 def welcome(req):
     return render(req,'mainapp/welcome.html')
@@ -8,7 +12,34 @@ def home(req):
     return render(req,'mainapp/Home.html')
 
 def signup(req):
+    if req.method == 'POST':
+        user_form = UserForm(data=req.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            user.save()
+            return HttpResponseRedirect(reverse('mainapp:login'))
+        else:
+            # One of the forms was invalid if this else gets called.
+            print(user_form.errors)
+    else:
+        # Was not an HTTP post so we just render the forms as blank.
+        user_form = UserForm()
     return render(req,'mainapp/signup.html')
 
-def login(req):
-    return render(req,'mainapp/login.html')
+def login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('welcome'))
+            else:
+                return HttpResponse("Your account is not active.")
+        else:
+            print("Someone tried to login and failed.")
+            print("They used username: {} and password: {}".format(username,password))
+            messages.error(request,'Incorrect username or password')
+            return redirect('mainapp:login')
+    return render(request,'mainapp/login.html')
